6c272a565db217ce641631bd8b169f92c6c6896f
advaith <thakku0001@gmail.com>
2025-11-28 14:27:24 +0000

    third commit

--- app/layout.js
+++ app/layout.js
@@ -3,7 +3,7 @@ import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
 
 export const metadata = {
-  title: "Thakku Portfolio",
+  title: "Advaith Portfolio",
 };
 
 export default function RootLayout({ children }) {
